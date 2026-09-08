4a7ce89e22f99f1e3441f768e5eac15e7aa097d8
Cesar <cesar.gtz.2017@gmail.com>
2026-09-08 16:36:28 -0600

    PHP para recibir formulario y su respuesta

---
+++ recibir.php
@@ -0,0 +1,10 @@
+<?php
+$nombre=$_POST['nombre'];
+$fecha_nacimiento=$_POST['fecha_nacimiento'];
+$correo=$_POST['correo'];
+
+
+
+echo "<p> Nombre: ".$nombre. "</p>";
+echo "<p> Correo: ".$correo. "</p>";
+echo "<p> Fecha de nacimiento: ".$fecha_nacimiento. "</p>"; 
