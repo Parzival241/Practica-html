177a0e4b2287fdab3637ded9c6ac521d7d62831c
Cesar Gutierrez <cesar.gtz.2017@gmail.com>
2026-09-23 20:04:37 -0600

    conexion creada a base de datos

---
+++ conexion.php
@@ -0,0 +1,16 @@
+  <?php
+  // Credenciales
+  $servername = "127.0.0.1";
+  $username = "root";
+  $password = "";
+  $database = "horoscopo";
+
+  try {
+    $conn = new PDO("mysql:host=$servername;port=3306;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+?>
--- recibir.php
+++ recibir.php
@@ -1,10 +1,25 @@
 <?php
-$nombre=$_POST['nombre'];
-$fecha_nacimiento=$_POST['fecha_nacimiento'];
-$correo=$_POST['correo'];
 
+  require('conexion.php');
 
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre=$_POST['nombre'];
+    $fecha_nacimiento=$_POST['fecha_nacimiento'];
+    $correo=$_POST['correo'];
 
-echo "<p> Nombre: ".$nombre. "</p>";
-echo "<p> Correo: ".$correo. "</p>";
-echo "<p> Fecha de nacimiento: ".$fecha_nacimiento. "</p>"; 
+  $sql = "INSERT INTO personas (nombre, correo, fecha_nacimiento)
+                    VALUES ('$nombre', '$correo', '$fecha_nacimiento')";
+            $conn->exec($sql);
+            // Mostrar información de tabla personas
+            $stmt = $conn->query("SELECT * FROM personas");
+            $personas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // Recorre el array de personas y muestra la información
+            foreach ($personas as $persona) {
+                echo "<p>Nombre: " . $persona['nombre'] . ", Correo: " . $persona['correo'] . ", Fecha de nacimiento: " . $persona['fecha_nacimiento'] . "</p>";
+            }
+        }
+        else {
+            echo "<p>No es una petición tipo POST.</p>";
+        }
+?>
